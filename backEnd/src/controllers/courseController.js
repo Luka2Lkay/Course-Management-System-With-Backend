50367a63a888496be3c20c43e364a5dfe3e0aab2
Luka <lukhanyo95@gmail.com>
2024-05-17 15:59:27 +0200

    comment

--- backEnd/src/controllers/courseController.js
+++ backEnd/src/controllers/courseController.js
@@ -1,25 +1,27 @@
 const Course = require("../models/course");
 
 exports.createCourse = async (req, res) => {
-  try {
-    const { course, description, modules, duration, availability } = req.body;
+  // try {
+  //   const { course, description, modules, duration, availability } = req.body;
 
-    const imageUrl = "https://course-management-system-with-backend-7u9c.vercel.app/" + req.file.filename;
+  //   const imageUrl = "https://course-management-system-with-backend-7u9c.vercel.app/" + req.file.filename;
 
-    const courseInfo = new Course({
-      course,
-      description,
-      modules,
-      duration,
-      availability,
-      imageUrl,
-    });
+  //   const courseInfo = new Course({
+  //     course,
+  //     description,
+  //     modules,
+  //     duration,
+  //     availability,
+  //     imageUrl,
+  //   });
 
-    const savedCourse = await courseInfo.save();
-    res.status(201).json(savedCourse);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  //   const savedCourse = await courseInfo.save();
+  //   res.status(201).json(savedCourse);
+  // } catch (error) {
+  //   res.status(500).json({ error: error.message });
+  // }
+
+  res.status(200).json({"message": "posted"})
 };
 
 exports.getAllCourses = async (_req, res) => {
