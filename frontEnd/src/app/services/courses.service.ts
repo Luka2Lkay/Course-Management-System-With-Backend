58e08989521b5df7d7c4f2838e5093091f7e4292
Luka <lukhanyo95@gmail.com>
2024-05-17 10:53:15 +0200

    remove forward slash at the end

--- frontEnd/src/app/services/courses.service.ts
+++ frontEnd/src/app/services/courses.service.ts
@@ -8,7 +8,7 @@ import { Course } from '../interfaces/course';
 })
 
 export class CoursesService {
-  apiUrl = 'https://course-management-system-with-backend.vercel.app/';
+  apiUrl = 'https://course-management-system-with-backend.vercel.app';
 
   constructor(private _http: HttpClient) {}
 
