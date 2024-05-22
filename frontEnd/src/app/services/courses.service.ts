85745e15b39f1398636f72901026721e6fde0f25
Luka <lukhanyo95@gmail.com>
2024-05-22 14:10:23 +0200

    update the link

--- frontEnd/src/app/services/courses.service.ts
+++ frontEnd/src/app/services/courses.service.ts
@@ -8,7 +8,7 @@ import { Course } from '../interfaces/course';
 })
 
 export class CoursesService {
-  apiUrl = 'https://course-management-system-with-backend-7u9c.vercel.app';
+  apiUrl = 'https://course-management-system-with-backend-qvqd.vercel.app/';
 
   constructor(private _http: HttpClient) {}
 
