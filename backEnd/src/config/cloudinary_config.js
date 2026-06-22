2dc2c022e0918ac6d0f769e0237ff80560f43acc
Luka <luka.matshebelele@gmail.com>
2026-06-22 14:44:10 +0200

    check if environment variables are being accessed

--- backEnd/src/config/cloudinary_config.js
+++ backEnd/src/config/cloudinary_config.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
+console.log("Cloudinary env check:", {
+  cloud_name: !!process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: !!process.env.CLOUDINARY_API_KEY,
+  api_secret: !!process.env.CLOUDINARY_API_SECRET,
+});
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
