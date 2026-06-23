6bdaf117acba06bf20c4d589b2fd29ad31e2ed1c
Luka <luka.matshebelele@gmail.com>
2026-06-23 18:33:49 +0200

    remove log

--- backEnd/src/helper_functions/multer.js
+++ backEnd/src/helper_functions/multer.js
@@ -2,9 +2,6 @@ const multer = require("multer");
 const cloudinary = require("../config/cloudinary_config");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-
-console.log("I am in multer")
-
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
