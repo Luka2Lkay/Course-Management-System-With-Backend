e49ac10190d3c24634c2f272b772c2c31f93158d
Luka <luka.matshebelele@gmail.com>
2026-06-23 08:42:31 +0200

    remove env variables check

--- backEnd/src/config/cloudinary_config.js
+++ backEnd/src/config/cloudinary_config.js
@@ -1,11 +1,5 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2;
-
-console.log("Cloudinary env check:", {
-  cloud_name: !!process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: !!process.env.CLOUDINARY_API_KEY,
-  api_secret: !!process.env.CLOUDINARY_API_SECRET,
-});
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
