eda19134fde5f5391b36c69e2b4e1b7d426ea786
Luka <luka.matshebelele@gmail.com>
2026-06-24 09:11:04 +0200

    remove unused component

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
 import { AddCourseComponent } from './add-course/add-course.component';
-import { MatDialog } from '@angular/material/dialog'
+import { MatDialog } from '@angular/material/dialog';
 import { CoursesService } from './services/courses.service';
-import { DialogRef } from '@angular/cdk/dialog';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'course-management-system';
-  constructor(private _dialog: MatDialog, private _courseService: CoursesService){
+  constructor(
+    private _dialog: MatDialog,
+    private _courseService: CoursesService,
+  ) {}
 
-  }
-
-  openForm(){
-    this._dialog.open(AddCourseComponent)
+  openForm() {
+    this._dialog.open(AddCourseComponent);
   }
 }
